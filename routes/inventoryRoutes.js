7f8e36d1cc131034ff1d829da97e6b4569b9b8d8
Boluwatife James <bosingwa100@gmail.com>
2024-12-11 15:23:37 +0100

    First Commit

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const router = express.Router()
+const inventoryController = require('../controllers/inventoryController');
+
+router.post('/create',inventoryController.createInventory);
+router.get('/allInventory',inventoryController.getAllInventory);
+router.get('/view/:inventoryId',inventoryController.viewInventory);
+router.put('/edit/:inventoryId',inventoryController.updateInventory);
+router.delete('/delete/:inventoryId',inventoryController.deleteInventory);
 
 
 module.exports = router
