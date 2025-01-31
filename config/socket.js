1024c9ba83259e5066c4af703f9351790ac3f0f2
Augustine Ofor <100804858+Iupac120@users.noreply.github.com>
2025-01-31 11:52:00 +0100

    updated the socket

--- config/socket.js
+++ config/socket.js
@@ -1,121 +1,7 @@
-// const {Server} = require("socket.io")
-// const onlineUser = []
-// const initializeSocket = (server) =>{
-//     const io = new Server(server,{
-//         cors:{
-//             origin:'http://localhost:5173',
-//             methods:["POST","GET"],
-//             credentials:true
-//         }
-//     })
-//     io.on("connection",(socket) =>{
-//         socket.on("register",(userId) =>{
-//             onlineUser.some(user =>user.userId === userId) &&
-//             onlineUser.push[{
-//                 userId,
-//                 socketId:socket.id
-//             }] 
-//             console.log(`User ${userId} connected with socket id: ${socket.id}`);
-//         })
-
-//         socket.on("disconnect",() =>{
-//             for (const [userId,socketId] of Object.entries(onlineUser)){
-//                 if(socketId === socket.id){
-//                     delete onlineUser(userId)
-//                     console.log(`User ${userId} disconnected`);
-//                 }
-//             }
-//         })
-//     })
-// return io
-    
-// }
-
-
-// const getUserSocketMap = () => userSocketMap
-
-
-
-// module.exports = {
-//   initializeSocket,
-//   getUserSocketMap,
-// };
-
-
-
-
-
-
-// const { Server } = require("socket.io");
-// let onlineUser = [];
-
-// const initializeSocket = (server) => {
-//   const io = new Server(server, {
-//     cors: {
-//       origin: "http://localhost:5173",
-//       methods: ["POST", "GET"],
-//       credentials: true,
-//     },
-//   });
-// //listen to new user register
-//   io.on("connection", (socket) => {
-//     socket.on("register", (userId) => {
-//       // Check if userId is not already in the array before adding
-//       const isUserOnline = onlineUser.some((user) => user.userId === userId);
-//       if (!isUserOnline) {
-//         onlineUser.push({
-//           userId,
-//           socketId: socket.id,
-//         });
-//         console.log(`User ${userId} connected with socket id: ${socket.id}`);
-//       }
-//       console.log("onlineUser",onlineUser)
-//       io.emit("getOnlineUser",onlineUser)
-//     });
-// //send new message
-//     socket.on("sendMessage",(message)=>{
-//       const user = onlineUser.find((user) =>user.userId === message.recipientId)//reciepientId is from the frontend
-//       if(user){
-//         io.to(user.socketId).emit("getMessage",message)
-//         io.to(user.socketId).emit("getNotification",{
-//           senderId:message.senderId,
-//           isRead:false,
-//           date:new Date()
-//         })
-//       }
-//     })
-
-// // receive message
-
-//     socket.on("disconnect", () => {
-//       // onlineUser.forEach((user, index) => {
-//       //   if (user.socketId === socket.id) {
-//       //     console.log(`User ${user.userId} disconnected`);
-//       //     onlineUser.splice(index, 1); // Remove the disconnected user
-//       //   }
-//       // });
-//       onlineUser = onlineUser.filter((user)=>user.socketId !== socket.id)
-//       io.emit("getOnlineUser",onlineUser)
-//     });
-//   });
-
-//   return io;
-// };
-
-
-// const getOnlineUsers = () => onlineUser;
-
-// module.exports = {
-//   initializeSocket,
-//   getOnlineUsers,
-// };
-
 
 const { Server } = require("socket.io")
 const { Message, Chat, User } = require("../models")
-
 let onlineUsers = []
-
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
@@ -149,6 +35,7 @@ const initializeSocket = (server) => {
           io.to(recipient.socketId).emit("getMessage", message)
         }
         io.to(socket.id).emit("messageSent", message)
+        console.log("message",message)
       } catch (error) {
         console.error("Error sending message:", error)
       }
@@ -176,6 +63,7 @@ const initializeSocket = (server) => {
         })
     
         io.to(socket.id).emit("groupCreated", groupChat)
+        console.log("group",groupChat)
       } catch (error) {
         console.error(error)
       }
